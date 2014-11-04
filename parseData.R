827cd75f28a910fd8b17a59482fb7554d6aabbb2
Andrew Kaluzny <akk2141@columbia.edu>
2014-11-04 16:51:19 -0500

    Wheeeeeeee! Parsing is done! Still doesn't scale up too well, so sampling is necessary to get anything done in a reasonable amount of time (perhaps at all--using apply-like functions may be sucking up more memory than necessary, and a for-loop could be more economical in that regard).
    Set up to save the data, so parsing only needs to be done once.

--- parseData.R
+++ parseData.R
@@ -1,22 +1,33 @@
-dataFile <- "data/webData.csv"
+dataDirectory <- "data"
+dataFile <- paste0(dataDirectory, "/webData.csv")
 
 colClasses <- c("integer", rep("character", 4), "integer", 
                 rep("character", 7), "integer", rep("character", 4))
-#ratingData <- read.csv(dataFile, header=TRUE, sep=",", quote="\"",
-#                       stringsAsFactors=FALSE,
-#                       colClasses=colClasses)
+print("Reading in data...")
+ratingData <- read.csv(dataFile, header=TRUE, sep=",", quote="\"",
+                       stringsAsFactors=FALSE,
+                       colClasses=colClasses)
 
 # Make sure the dates are understood in the right format
+print("Converting dates...")
 convertDate <- function(s) { return(as.Date(s, format="%m/%d/%Y")) }
 ratingData$INSPECTION.DATE <- convertDate(ratingData$INSPECTION.DATE)
 ratingData$GRADE.DATE <- convertDate(ratingData$GRADE.DATE)
 ratingData$RECORD.DATE <- convertDate(ratingData$RECORD.DATE)
 
-# make empty violation
-
-# Get rid of inspections with a date of 1900-01-01, as these have 
+# Get rid of inspections with a date of 1900-01-01, as these are for 
 # restaurants not been inspected yet
 ratingData <- ratingData[ratingData$INSPECTION.DATE != "1900-01-01", ]
+
+# create a subset of the data, to make testing go faster
+print("Taking sample...")
+uniqueRestaurants <- unique(ratingData$CAMIS)
+nRestaurants <- length(uniqueRestaurants)
+fraction <- 0.025
+sampleVector <- sample(uniqueRestaurants, as.integer(fraction * nRestaurants))
+ratingData <- ratingData[ratingData$CAMIS %in% sampleVector, ]
+# create a random testID from the sample
+testID <- sample(ratingData$CAMIS, 1)
 
 # Check to see that addresses are uniquely associated with the CAMIS ids
 # note this is not optimized and will take a while to run
@@ -42,13 +53,16 @@ inspectionColumns <- c("CAMIS", "INSPECTION.DATE", "ACTION", "SCORE",
                        "GRADE", "GRADE.DATE", "RECORD.DATE", "INSPECTION.TYPE")
 
 # create a data frame that contains just the restaurant information
+print("Creating restaurant data frame...")
 restaurants <- ratingData[!duplicated(ratingData$CAMIS), restaurantColumns]
 
 # create a frame with a row for each inspection
+print("Creating inspection data frame...")
 inspectionRows <- !duplicated(paste(ratingData$CAMIS, ratingData$INSPECTION.DATE))
 inspections <- ratingData[inspectionRows, inspectionColumns]
 
 # add column to restaurant frame for the number of inspections
+print("Adding number of inspections column...")
 restaurants$N.INSPECTIONS <- vapply(restaurants$CAMIS,
                                     function(id) length(inspections[inspections$CAMIS==id, 1]),
                                     FUN.VALUE=integer(1))
@@ -65,6 +79,7 @@ makeViolationString <- function(id, date) {
 #                                 inspections$CAMIS, inspections$INSPECTION.DATE,
 #                                 SIMPLIFY="vector")
 
+print("Creating matrix of restaurant violations...")
 violationCodes <- unique(ratingData$VIOLATION.CODE)
 violationCodes <- violationCodes[violationCodes != ""] # exclude the empty violation
 nCodes <- length(violationCodes)
@@ -77,5 +92,67 @@ makeViolationVector <- function(violation) {
                    FUN.VALUE=integer(1))
   return(result / restaurants$N.INSPECTIONS)
 }
-violation <- sapply(violationCodes, makeViolationVector, simplify="matrix")
-#names(violationVectors) <- restaurants$CAMIS
+violationMatrix <- sapply(violationCodes, makeViolationVector, simplify="matrix")
+rownames(violationMatrix) <- restaurants$CAMIS
+
+# helper function to go from score to grade--A -> 1, etc.
+getGrade <- function(score) {
+  result <- as.integer(2 + 0 * score)
+  result[score <= 13] <- 1
+  result[score >= 28] <- 3
+  return(result)
+}
+
+# Create matrix of transition matrices for each restaurant,
+# also create a column in the inspection frame for the time since last inspection
+print("Creating transition matrices...")
+inspections$SINCE.LAST <- numeric(nrow(inspections))
+# we'll construct the transpose, as its easier to build up by columns of 
+# transition matrices
+transitionMatrices <- matrix(, nrow=9, ncol=nrow(restaurants))
+rownames(transitionMatrices) <- c("AA", "AB", "AC", 
+                                  "BA", "BB", "BC",
+                                  "CA", "CB", "CC")
+colnames(transitionMatrices) <- restaurants$CAMIS
+# sort the inspection table
+inspections <- inspections[order(inspections$CAMIS, inspections$INSPECTION.DATE), ]
+# loop over each restaurant and fill in the matrices and column
+for (id in restaurants$CAMIS) {
+  insp <- inspections[inspections$CAMIS==id, c("SCORE", "INSPECTION.DATE")]
+  sinceLast <- numeric(nrow(insp))
+  sinceLast[1] <- NA # no time since last for the first inspection
+  transMatrix <- numeric(9)
+  grades <- getGrade(insp[, "SCORE"])
+  n <- length(grades)
+  if (n > 1) {
+    for (ii in 2:n) {
+      lastGrade <- grades[ii-1]
+      grade <- grades[ii]
+      transMatrix[(lastGrade-1)*3 + grade] <- transMatrix[(lastGrade-1)*3 + grade] + 1
+      sinceLast[ii] <- difftime(insp[ii, 2], insp[ii-1, 2], units="days")
+    }
+    ## do the scaling for each "column" of the transition matrix
+    for (ii in 1:3) {
+      nGrade <- sum(grades==ii, na.rm=TRUE)
+      if (nGrade > 0) {
+        transMatrix[(1:3) + (ii-1)*3] <-  transMatrix[(1:3) + (ii-1)*3] / nGrade
+      }
+    }
+  }
+  transitionMatrices[, as.character(id)] <- transMatrix
+  inspections[inspections$CAMIS==id, "SINCE.LAST"] <- sinceLast
+  rm(insp, grades, transMatrix, sinceLast) # might not be needed
+}
+transitionMatrices <- t(transitionMatrices)
+
+print("Saving data...")
+write.table(inspections, paste0(dataDirectory, "/inspectionData.csv"), 
+            row.names=FALSE, sep=",")
+write.table(restaurants, paste0(dataDirectory, "/restaurantData.csv"), 
+            row.names=FALSE, sep=",")
+write.table(violationMatrix, paste0(dataDirectory, "/violationData.csv"), 
+            row.names=TRUE, sep=",")
+write.table(transitionMatrices, paste0(dataDirectory, "/transitionData.csv"), 
+            row.names=TRUE, sep=",")
+
+print("Done.")
